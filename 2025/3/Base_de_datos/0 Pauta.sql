6dbee5ccafb890f2fe199346dc116a64fda88b75
Rodrigo Reyess <rodrigo.reyess@utem.cl>
2025-11-08 00:04:00 -0300

    Informes para revisiòn

---
+++ 2025/3/Base_de_datos/0 Pauta.sql
@@ -0,0 +1,27 @@
+-- 0. Crear BDD
+DROP DATABASE IF EXISTS Banco;
+CREATE DATABASE Banco;
+USE Banco;
+
+-- 1. Crear tablas:
+-- ESTADO
+-- ENTIDAD
+-- SUCURSAL
+-- SITUACION
+-- PERSONA
+-- CUENTA
+-- TARJETA
+
+-- 2. Crear restricciones FOREIGN KEY
+
+-- 3. Crear secuencias
+
+-- 4. Insertar registros en ESTADO usando la secuencia
+-- 5. Insertar registros en SITUACION usando la secuencia
+-- 6. Insertar registros en ENTIDAD usando la secuencia
+-- 7. Insertar registros en SUCURSAL usando la secuencia
+-- 8. Insertar registros en las tablas vacías (PERSONA, CUENTA, TARJETA)
+-- Confirmar los cambios
+-- Verificar los registros insertados
+
+DROP DATABASE IF EXISTS Banco;
